6a0d53b3268667062ce9c12f8c6d363aebb1c051
Pavan Navarathna Devaraj <pavannd1@gmail.com>
2017-11-12 12:08:21 -0800

    Create RemoveElement.cc

---
+++ LeetCode/Easy/RemoveElement.cc
@@ -0,0 +1,31 @@
+/*
+ * Given an array and a value, remove all instances of that value in-place and return the new length.
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ * Example:
+ *  Given nums = [3,2,2,3], val = 3,
+ *  Your function should return length = 2, with the first two elements of nums being 2.
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        auto length = nums.size();
+        while (i < length) {
+            if (nums[i] == val) {
+                // Swap with the last element
+                nums[i] = nums[length -1];
+                length--;
+            } else {
+                i++;
+            }
+        }
+        return length;
+    }
+};
