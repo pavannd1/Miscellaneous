ca9c4d8ef3d484adf1e0959ac0c9e8707c8d2b70
Pavan Navarathna Devaraj <pavannd1@gmail.com>
2017-12-26 21:12:46 -0800

    Create DetectCycle.cc

---
+++ LeetCode/LinkedList/Easy/DetectCycle.cc
@@ -0,0 +1,31 @@
+/**
+ * Given a linked list, determine if it has a cycle.
+ * Avoid using extra space.
+ */
+
+#include <iostream>
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        auto slow = head;
+        auto fast = head;
+        
+        while (slow && fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
